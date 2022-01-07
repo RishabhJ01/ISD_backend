a123d32b2be735ca4deb92900faf68b592040a9d
Reeshabh Jain <rishabhrox2001@gmail.com>
2022-01-07 23:20:06 +0530

    first change

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/",(req,res)=>{
     res.send("welcome to home page")
 })
 
-
-app.listen(8080,()=>{
-    console.log("Server started on port 8080");
+const port = process.env.PORT || 8080;
+app.listen(port,()=>{
+    console.log(`Server started on http://localhost:${port}/`);
 })
